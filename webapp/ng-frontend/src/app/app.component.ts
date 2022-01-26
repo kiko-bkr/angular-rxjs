e144244432f4b0138534f225107a0820dc6e1be9
kiko-bkr <bokorkrisztian7@gmail.com>
2022-01-26 12:37:48 +0200

    Added reactive button listeners

--- webapp/ng-frontend/src/app/app.component.ts
+++ webapp/ng-frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, timer, map, takeUntil } from 'rxjs';
+import { Observable, timer, map, takeUntil, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,17 @@ import { Observable, timer, map, takeUntil } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-frontend';
+  title: string = 'ng-frontend';
+
+  buttonLabels: string[] = ['A', 'B', 'C'];
+
+  labelStream: Subject<string> = new Subject<string>();
+  labels$: Observable<string> = this.labelStream.asObservable();
 
   timer$: Observable<number> = timer(11000);
-
   counter$: Observable<number> = timer(0, 1000).pipe(map(n => 10 - n), takeUntil(this.timer$));
+
+  handleClick(label: string): void {
+    this.labelStream.next(label);
+  }
 }
